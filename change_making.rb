5c99d5a2a4e1c5dd2e77ee066184e5dd7f17b5f4
nandini0407 <nandini0407@gmail.com>
2017-04-05 12:12:48 -0700

    add change making problem to algos

--- change_making.rb
+++ change_making.rb
@@ -1,2 +1,19 @@
 def make_change(deno, change)
+  f = [0]
+  deno.unshift(0)
+  i = 1
+  while i <= change
+    temp = Float:: INFINITY
+    j = 1
+    while j < deno.length && i >= deno[j]
+      temp = [f[i - deno[j]], temp].min
+      j += 1
+    end
+    f << (temp + 1)
+    i += 1
+  end
+  f[change]
 end
+
+puts make_change([1, 3, 4], 6)
+puts make_change([1, 7, 10], 14)
